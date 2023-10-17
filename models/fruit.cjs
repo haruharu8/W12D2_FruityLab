6e20dd7cc498e426be52d1b76500e7a1fde4f3e9
Kelly Siu <ksiu@LABA.PSWAS.LOCAL>
2023-10-17 16:02:50 -0700

    finish setting up db

--- models/fruit.cjs
+++ models/fruit.cjs
@@ -1,24 +1,19 @@
-// create schema (breakdown of what our data should look like)
 const mongoose = require('mongoose');
-
+// create schema (breakdown of what our data should look like)
 const fruitSchema = new mongoose.Schema(
-{
-    name: String,
-    color: String,
-    readyToEat: Boolean
-},
-{
-    timestamps: true,
-
-}
-);
-
-// create Model w/ schema
-// whatever we put as the collection name will be lowercased and plurlized +s
+    {
+        name: String,
+        color: String,
+        readyToEat: Boolean
+    },
+    {
+        timestamps: true
+    }
+    )
+// create Model with that schema
+// whatever we put as the collection name will be lowecased and pluralized +s
 // Fruit > fruits
 // User > users
-const Fruit = mongoose.model('Fruit',            fruitSchema)
-
-//                          1.   ^ which collection 2. ^ the schema
-
+const Fruit = mongoose.model("Fruit",         fruitSchema);
+//                          1. ^ which collection   2. ^ the schema
 module.exports = Fruit;
--- server.cjs
+++ server.cjs
@@ -1,20 +1,20 @@
 /* eslint-disable no-undef */
-const express = require('express') 
-const path = require('path') 
+const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const morgan = require('morgan')
 const PORT = 3000;
 const app = express();
 
-// allows us to use proces.env (get variables from .env file)
+const Fruit = require('./models/Fruit.cjs')
+
+
+// allows us to use process.env (get variables from .env file)
 require('dotenv').config();
-require('./config/db.cjs')
 
 
-const middleware = (req, res, next) => {
-    console.log("doing stuff");
-    next();
-}
+require('./config/db.cjs');
+
 
 app.use(cors({
     origin: "*"
@@ -22,27 +22,32 @@ app.use(cors({
 
 app.use(morgan('dev'))
 
-app.use(middleware);
+
+
+
+
 
 app.use(express.json()); // adds .body to the request
 
-// "/"
-// serve the html and js of our react app (dist folder)
 
-const fruits = [];
 
-app.get('/fruits', (req,res) => {
-    res.send(fruits)
+
+app.get("/fruits", async (req, res) => {
+    let fruitsFromDB = await Fruit.find();
+    res.send(fruitsFromDB);
 })
 
-app.post('/fruits', (req,res) => {
-    fruits.push(req.body);
-    res.status(201).send(req.body);
-})
 
 app.get("/", (req, res) => {
     res.send("here is your valuable data")
-    
+})
+
+app.post("/fruits", async (req,res) => {
+    console.log(req.body);
+    let fruit = req.body;
+    let responseFromDB = await Fruit.create(fruit);
+    console.log(responseFromDB);
+    res.send("Route is good")
 })
 
 
